249680e42a510f9706e1bfea5153fee57408feb1
Aziz H <zakaziz86@gmail.com>
2020-11-10 00:50:50 +0700

    mencoba axios dan json-server

---
+++ users-app.js
@@ -0,0 +1,9 @@
+const users = require('./users')
+
+async function showData() {
+    let res = await users.all()
+    console.log(res.data)
+}
+
+showData()
+console.log('finished')
---
+++ users.test.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+const Users = require('./users')
+
+jest.mock('axios')
+
+test('should fetch users', () => {
+    const users = [{
+            "id": 1,
+            "first_name": "Robert",
+            "last_name": "Scwartz",
+            "email": "rob23@gmail.com"
+        }, {
+            "id": 2,
+            "first_name": "Lucy",
+            "last_name": "Ballmer",
+            "email": "lucy56@gmail.com"
+        }]
+
+        const resp = { data : users }
+        axios.get.mockImplementation(() => Promise.resolve(resp))
+        Users.all().then(resp => expect(resp.data).toEqual(users))
+})
